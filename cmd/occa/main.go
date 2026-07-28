98c047f87bb3cea38b4d010b1e6391ec126395f0
Kuma <kumastation2001@gmail.com>
2026-07-28 21:50:40 +0000

    fix: wire main.go, remove dead code in streamer, add edit retry on 429

--- cmd/occa/main.go
+++ cmd/occa/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/anggasct/occa/internal/channel"
+	"github.com/anggasct/occa/internal/channel/discord"
+	"github.com/anggasct/occa/internal/channel/telegram"
 	"github.com/anggasct/occa/internal/config"
 	"github.com/anggasct/occa/internal/relay"
+	"github.com/anggasct/occa/internal/render"
+	"github.com/anggasct/occa/internal/router"
+	"github.com/anggasct/occa/internal/store"
 )
 
 func main() {
@@ -25,9 +34,52 @@ func main() {
 	}
 	slog.SetDefault(slog.New(handler))
 
-	_ = relay.NewHTTPClient(cfg.OpenCodeAddr)
+	db, err := store.Open(cfg.DBPath)
+	if err != nil {
+		slog.Error("failed to open store", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	relayClient := relay.NewHTTPClient(cfg.OpenCodeAddr)
+	resolver := relay.NewSessionResolver(db.SessionRepo(), relayClient)
+	renderer := render.New()
+	rt := router.New(relayClient, db, resolver)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	var channels []channel.Channel
+
+	if cfg.TelegramToken != "" {
+		tg := telegram.New(cfg.TelegramToken)
+		channels = append(channels, tg)
+	}
+	if cfg.DiscordToken != "" {
+		dc := discord.New(cfg.DiscordToken)
+		channels = append(channels, dc)
+	}
+
+	_ = renderer
+
+	for _, ch := range channels {
+		go func(c channel.Channel) {
+			slog.Info("starting channel", "platform", c.Name())
+			if err := c.Start(ctx, func(msg channel.IncomingMessage) {
+				if err := rt.Route(ctx, msg); err != nil {
+					slog.Error("route error", "platform", msg.Platform, "error", err)
+				}
+			}); err != nil {
+				slog.Error("channel error", "platform", c.Name(), "error", err)
+			}
+		}(ch)
+	}
 
 	slog.Info("occa started", "opencode_addr", cfg.OpenCodeAddr)
 
-	select {}
+	<-ctx.Done()
+	slog.Info("shutting down")
+	for _, ch := range channels {
+		ch.Stop()
+	}
 }
